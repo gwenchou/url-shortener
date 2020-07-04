20efa087a92f94bd80be478d4c401df850fc7635
gwenchou <gwen@gwendesign.net>
2020-07-04 13:57:04 +0800

    Create shortened URL retrieve API

--- src/url_shortener/tests.py
+++ src/url_shortener/tests.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
 from django.test import TestCase, Client
+from url_shortener.models import Shortened_Url
 
-# Create your tests here.
+
 class UrlShortenerTestCase(TestCase):
     def setUp(self):
         pass
@@ -10,9 +11,20 @@ class UrlShortenerTestCase(TestCase):
         origin_url = 'https://google.com'
         http_client = Client()
         response = http_client.post('/shortened_urls/', {'origin_url': origin_url})
-        print(response.content)
         response_content = response.json()
 
-        # self.assertEqual(response_content['origin_url'], origin_url)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertEqual(response_content['origin_url'], origin_url)
+
+    def test_retrieve_shortened_url(self):
+        origin_url = 'https://google.com'
+        shortened_url = Shortened_Url.objects.create(
+            origin_url=origin_url,
+        )
+
+        http_client = Client()
+        response = http_client.get('/shortened_urls/{}/'.format(shortened_url.alias))
+        response_content = response.json()
+        
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response_content['origin_url'], origin_url)
--- src/url_shortener/views.py
+++ src/url_shortener/views.py
@@ -3,11 +3,8 @@ from django.views import View
 from django.http import JsonResponse
 from url_shortener.models import Shortened_Url
 
-# Create your views here.
-class UrlShortener(View):
-    def get(self, request):
-        pass
 
+class UrlShortener(View):
     def post(self, request):
         shortened_url = Shortened_Url.objects.create(
             origin_url=request.POST['origin_url']
@@ -17,3 +14,11 @@ class UrlShortener(View):
             'alias': shortened_url.alias,
             'origin_url': shortened_url.origin_url
         }, status=HTTPStatus.CREATED)
+
+    def get(self, request, slug):
+        shortened_url = Shortened_Url.objects.get(alias=slug)
+        
+        return JsonResponse({
+            'alias': shortened_url.alias,
+            'origin_url': shortened_url.origin_url
+        }, status=HTTPStatus.OK)
